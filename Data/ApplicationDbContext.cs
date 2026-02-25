06420ed9f5312f1ea350a5512ba60a998a2e5de4
Chris Phillips <cp.phillips15@gmail.com>
2026-02-25 17:51:17 +0000

    Configured the entity framework with SQLite and ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHub.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
