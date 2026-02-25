0ade3e63d9c34855f441fbef6ac56c9155acab21
Chris Phillips <cp.phillips15@gmail.com>
2026-02-25 18:30:20 +0000

    Started logic of account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using BookHub.Models;
+using BookHub.DTOs;
+
+namespace BookHub.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        
+    }
+}
