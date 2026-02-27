ddf72dcf47d7785a7aa8bd26dee6aa14cb5e2974
Chris Phillips <cp.phillips15@gmail.com>
2026-02-27 16:19:22 +0000

    added comments planning out all relationships before proceeding

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -23,29 +23,42 @@ namespace BookHub.Models
 
             // Many-to-Many relations
                 
+            // Link Table between Book and Genre
             builder.Entity<BookGenre>().HasKey(bg => new { bg.BookId, bg.GenreId });
                 // Composite primary key
 
+            // BookGenre --- Book
             builder.Entity<BookGenre>()
                 .HasOne(bg => bg.Book)
                 .WithMany(b => b.BookGenres)
                 .HasForeignKey(bg => bg.BookId);
 
+            // BookGenre --- Genre
             builder.Entity<BookGenre>()
                 .HasOne(bg => bg.Genre)
                 .WithMany(g => g.BookGenres)
                 .HasForeignKey(bg => bg.GenreId);
 
             // Many-to-One relations
+            // ReadingListItem --- ReadingList
             builder.Entity<ReadingListItem>()
                 .HasOne(rli => rli.ReadingList)
                 .WithMany(rl => rl.Items)
                 .HasForeignKey(rli => rli.ReadingListId);
 
+            // ReadingListItem --- Book
             builder.Entity<ReadingListItem>()
                 .HasOne(rli => rli.Book)
                 .WithMany() // Don't think back reference is needed?
                 .HasForeignKey(rli => rli.BookId);
+
+            // Book --- Author
+
+            // Review --- Book
+            
+            // Review --- User
+
+            
         }
     }
 }
