8e3459dc74cccf1227b428c4101060c0d0664d78
Chris Phillips <cp.phillips15@gmail.com>
2026-02-28 16:21:16 +0000

    added OnDelete behaviours to database models to account for non cascading delte errors

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -21,7 +21,9 @@ namespace BookHub.Models
         {
             base.OnModelCreating(builder);
 
+            //-----------------------
             // Many-to-Many relations
+            //-----------------------
                 
             // Link Table between Book and Genre
             builder.Entity<BookGenre>().HasKey(bg => new { bg.BookId, bg.GenreId });
@@ -31,44 +33,53 @@ namespace BookHub.Models
             builder.Entity<BookGenre>()
                 .HasOne(bg => bg.Book)
                 .WithMany(b => b.BookGenres)
-                .HasForeignKey(bg => bg.BookId);
+                .HasForeignKey(bg => bg.BookId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             // BookGenre --- Genre
             builder.Entity<BookGenre>()
                 .HasOne(bg => bg.Genre)
                 .WithMany(g => g.BookGenres)
-                .HasForeignKey(bg => bg.GenreId);
+                .HasForeignKey(bg => bg.GenreId)
+                .OnDelete(DeleteBehavior.Restrict);
 
+            // ---------------------
             // Many-to-One relations
+            // ---------------------
             // ReadingListItem --- ReadingList
             builder.Entity<ReadingListItem>()
                 .HasOne(rli => rli.ReadingList)
                 .WithMany(rl => rl.Items)
-                .HasForeignKey(rli => rli.ReadingListId);
+                .HasForeignKey(rli => rli.ReadingListId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             // ReadingListItem --- Book
             builder.Entity<ReadingListItem>()
                 .HasOne(rli => rli.Book)
                 .WithMany(b => b.ReadingListItems) //TODO add this attribute to Book
-                .HasForeignKey(rli => rli.BookId);
+                .HasForeignKey(rli => rli.BookId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             // Book --- Author
             builder.Entity<Book>()
                 .HasOne(b => b.Author)
                 .WithMany(a => a.Books)
-                .HasForeignKey(b => b.AuthorId);
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             // Review --- Book
             builder.Entity<Review>()
                 .HasOne(r => r.Book)
                 .WithMany(b => b.Reviews)
-                .HasForeignKey(r => r.BookId);
+                .HasForeignKey(r => r.BookId)
+                .OnDelete(DeleteBehavior.Cascade);
             
             // Review --- User
             builder.Entity<Review>()
                 .HasOne(r => r.User)
                 .WithMany(u => u.Reviews)
-                .HasForeignKey(r => r.UserId);
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
