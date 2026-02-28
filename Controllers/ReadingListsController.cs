c9f8ca0b327ba725c4f83f7f16de4ace47cd0b5e
Chris Phillips <cp.phillips15@gmail.com>
2026-02-28 12:52:06 +0000

    added controller end points for ReadingList. Noticed issues of updateReadingList potentially updateing userId, and the lack of meaninful attributes for the reading list entity. Added attribbutes to ReadingList Model, and updated migration. Put logic to be updated and tested

--- Controllers/ReadingListsController.cs
+++ Controllers/ReadingListsController.cs
@@ -15,10 +15,80 @@ namespace BookHub.Controllers
             _context = context;
         }
 
-        //TODO GET: api/readinglists
-        //TODO GET: api/readinglists/{listId}
-        //TODO POST: api/readinglists
-        //TODO PUT: api/readinglists/{listId}
-        //TODO DELETE: api/readinglists/{listId}
+        // GET: api/readinglists
+        [HttpGet]
+        public async Task<IActionResult> GetReadingLists()
+        {
+            var lists = await _context.ReadingLists
+                .Include(rl => rl.User)
+                .Include(rl => rl.Items)
+                    .ThenInclude(item => item.Book)
+                .ToListAsync();
+
+            return Ok(lists);
+        }
+
+        // GET: api/readinglists/{listId}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReadingList(int id)
+        {
+            var list = await _context.ReadingLists
+                .Include(rl => rl.User)
+                .Include(rl => rl.Items)
+                    .ThenInclude(item => item.Book)
+                .FirstOrDefaultAsync(rl => rl.Id == id);
+
+            if (list == null)
+                return NotFound();
+
+            return Ok(list);
+        }
+
+        // POST: api/readinglists
+        [HttpPost]
+        public async Task<IActionResult> CreateReadingList(ReadingList readingList)
+        {
+            _context.ReadingLists.Add(readingList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetReadingList), new { id = readingList.Id }, readingList);
+        }
+
+        // PUT: api/readinglists/{listId}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReadingList(int id, ReadingList updatedList)
+        {
+            if (id != updatedList.Id)
+                return BadRequest();
+
+            var list = await _context.ReadingLists.FindAsync(id);
+
+            if (list == null)
+                return NotFound();
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/readinglists/{listId}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReadingList(int id)
+        {
+            var list = await _context.ReadingLists
+                .Include(rl => rl.Items)
+                .FirstOrDefaultAsync(rl => rl.Id == id);
+
+            if (list == null)
+                return NotFound();
+            
+            // Remove link table entries first
+            _context.ReadingListItems.RemoveRange(list.Items);
+
+            _context.ReadingLists.Remove(list);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
--- Models/ReadingList.cs
+++ Models/ReadingList.cs
@@ -6,14 +6,15 @@ namespace BookHub.Models
     {
         public int Id {get;set;}
 
+        public string Name {get;set;}
+        public string Description {get;set;}
+        public bool IsPublic {get;set;}
+
         public string UserId {get;set;}
         public ApplicationUser User {get;set;}
 
         //Added initialized value to prevent null reference
         // TODO test for potential bugs caused by this
         public List<ReadingListItem> Items {get;set;} = new List<ReadingListItem>();
-        //Using RedingListItem instead, but left for now
-        //public List<Book> Book {get;set;}
-        
     }
 }
