23b12ec2eb14a187408a29f94d53ed3e64729d80
Chris Phillips <cp.phillips15@gmail.com>
2026-02-28 11:35:35 +0000

    added controller end points for Books

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -14,12 +14,73 @@ namespace BookHub.Controllers
         {
             _context = context;
         }
-    }
 
-    //TODO GET: api/books
-    //TODO GET: api/books/{bookId}
-    //TODO POST: api/books
-    //TODO PUT: api/books/{bookId}
+    // GET: api/books
+    [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.BookGenres)
+                    .ThenInclude(bg => bg.Genre)
+                .ToListAsync();
+            
+            return Ok(books);
+        }
+
+    // GET: api/books/{bookId}
+    [HttpGet]
+        public async Task<IActionResult> GetBook(int id)
+        {
+            var book = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.BookGenres)
+                    .ThenInclude(bg => bg.Genre)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            if (book == null) return NotFound();
+
+            return Ok(book);
+        }
+
+
+    // POST: api/books
+    [HttpPost]
+    public async Task<IActionResult> CreateBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return Ok(book);
+        }
+
+    // PUT: api/books/{bookId}
+    [HttpPut]
+    public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
+        {
+            var book =  await _context.Books.FindAsync(id);
+            if (book == null) return NotFound();
+
+            book.Title = updatedBook.Title;
+            book.Description = updatedBook.Description;
+            book.AuthorId = updatedBook.AuthorId;
+            // TODO add updating genre
+
+            await _context.SaveChangesAsync();
+            return Ok(book);
+        }
+
     //TODO DELETE: api/books/{bookId}
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) return NotFound();
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
 
 }
